1abd3ef88f5c763a81e206cb01431d4bf12b784d
Matt Kalinowski <mkalinowski@pivotal.io>
2017-09-08 07:47:09 -0700

    env vars for %-full calibration

--- cafe.py
+++ cafe.py
@@ -3,9 +3,6 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-EMPTY_POT_GRAMS = 1800
-FULL_POT_GRAMS = 3300
-
 devices = {}
 allStates = [
             {
@@ -54,13 +51,21 @@ def report():
 def allstates():
     return jsonify(allStates)
 
+@app.route('/raw')
+def raw():
+    return jsonify(devices)
+
+def qty_to_percent(qty_grams):
+    devLevel = (qty_grams - EMPTY_POT_GRAMS)/(FULL_POT_GRAMS - EMPTY_POT_GRAMS)
+    devLevel = int(round(devLevel * 100, 0))
+    devLevel = min(max(devLevel, 0), 100)
+    return devLevel
+
 @app.route('/status')
 def status():
     devFormatted = []
     for devId, qty in devices.items():
-        devLevel = (qty - EMPTY_POT_GRAMS)/(FULL_POT_GRAMS - EMPTY_POT_GRAMS)
-        devLevel = int(round(devLevel * 100, 0))
-        devLevel = min(max(devLevel, 0), 100)
+        devLevel = qty_to_percent(qty)
         devStatus = "AVAILABLE" if (devLevel > 0) else "UNAVAILABLE"
         devFormatted.append({
             "devId": devId,
@@ -74,8 +79,12 @@ if __name__ == "__main__":
     if os.environ.get('VCAP_SERVICES') is None: # running locally
         PORT = 5000
         DEBUG = True
+        EMPTY_POT_GRAMS = 1800
+        FULL_POT_GRAMS = 3300
     else:                                       # running on CF
         PORT = int(os.getenv("PORT"))
         DEBUG = False
+        EMPTY_POT_GRAMS = int(os.getenv("EMPTY_POT_GRAMS"))
+        FULL_POT_GRAMS = int(os.getenv("FULL_POT_GRAMS"))
 
     app.run(host='0.0.0.0', port=PORT, debug=DEBUG)
